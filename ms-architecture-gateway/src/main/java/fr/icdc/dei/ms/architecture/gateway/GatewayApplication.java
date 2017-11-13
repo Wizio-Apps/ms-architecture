4d5b0a1dd278ced47df0f01319a6422daf3beaf5
XtremZ <meidi.airouche@gmail.com>
2017-11-14 00:31:01 +0100

    Modification de l'annotation Eureka Client : utilisation de celle de Netflix au lieu de celle de Spring Cloud pour ne pas utiliser Ribbon par défaut

--- ms-architecture-gateway/src/main/java/fr/icdc/dei/ms/architecture/gateway/GatewayApplication.java
+++ ms-architecture-gateway/src/main/java/fr/icdc/dei/ms/architecture/gateway/GatewayApplication.java
@@ -2,7 +2,7 @@ package fr.icdc.dei.ms.architecture.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -12,7 +12,7 @@ import fr.icdc.dei.ms.architecture.gateway.filter.PreFilter;
 import fr.icdc.dei.ms.architecture.gateway.filter.RouteFilter;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
 
